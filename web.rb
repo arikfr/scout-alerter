65009bce77fe48b3981056b84dbdd174b90a00e3
Arik Fraimovich <arik@arikfr.com>
2012-08-16 21:31:13 +0300

    Added message parsing and sending

--- web.rb
+++ web.rb
@@ -1,9 +1,17 @@
 require 'sinatra'
+require 'json'
+require_relative 'sms.rb'
 
 get '/' do
     "Hello, world"
 end
 
 post '/scout' do
-  puts "received: #{params[:payload]}"
+  payload = params[:payload]
+  states = {"end"=>"Ended", "start"=>"started"}
+  if payload
+    json = JSON.parse(payload)
+    message = "#{json['server_hostname']}|#{json['plugin_name']}|#{states[json['lifecycle']]}: " + json['title'].strip
+    send_message('972542186395', message)
+  end
 end
