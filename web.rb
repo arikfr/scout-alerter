9981d323ec27610aaf6f078fc1bc36282de86ff1
Arik Fraimovich <arik@arikfr.com>
2012-08-21 22:19:15 +0300

    Cleaned up the code

--- web.rb
+++ web.rb
@@ -4,40 +4,27 @@ require 'json'
 require 'redis'
 require 'boxcar_api'
 
-require_relative 'sms.rb'
 require_relative './lib/alert.rb'
 
 uri = URI.parse(ENV["REDISTOGO_URL"])
 REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 
 def notify(message)
-  #recipients = REDIS.get('recipients').split(',')
-  #recipients.each do |recipient|
-    #send_message(recipient, message)
-  #end
-  #send_message(recipients.first, message)
-
   provider = BoxcarAPI::Provider.new(ENV['BOXCAR_KEY'], ENV['BOXCAR_SECRET'])
   provider.broadcast(message)
 end
 
 get '/' do
-  "Hello, world"
+  "Scout Alerter."
 end
 
 get '/test_message' do
-  if params[:arik]
-    provider = BoxcarAPI::Provider.new(ENV['BOXCAR_KEY'], ENV['BOXCAR_SECRET'])
-    provider.broadcast('☁☂ Scout Alerter: test message (let Arik know you received it).')
-  else
-    notify('☁☂ Scout Alerter: test message (let Arik know you received it).')
-  end
+  notify('☁☂ Scout Alerter: test message (let Arik know you received it).')
 end
 
 post '/scout' do
   payload = params[:payload]
   filtered_hostnames = REDIS.get('filtered_hostnames').split(',')
-  recipients = REDIS.get('recipients').split(',')
   if payload
     alert = Alert.new(payload)
     notify(alert.message) if alert.needs_delivery?(filtered_hostnames)
